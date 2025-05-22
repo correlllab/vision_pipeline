c53f01c427d8128d4b6d42d7afd457298943f7f7
maxconway <maxlconway@gmail.com>
2025-05-22 14:20:00 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,34 @@
+#!/usr/bin/env python3
+import os
 from setuptools import find_packages, setup
 
 package_name = 'vision_pipeline'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # include configuration file
+        ('share/' + package_name, [os.path.join('vision_pipeline', 'config.json')]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='max',
     maintainer_email='maxlconway@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Vision Pipeline ROS2 package',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'capture_cameras = vision_pipeline.capture_cameras:main',
+            'foundation_models = vision_pipeline.foundation_models:main',
+            'subscriber = vision_pipeline.subscriber:main',
+            'visionpipeline = vision_pipeline.visionpipeline:main',
         ],
     },
 )
+
