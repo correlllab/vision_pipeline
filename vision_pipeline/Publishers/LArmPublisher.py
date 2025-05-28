9dd6f3ac3b9a1995f3d4c5b94d9794540981d40e
humanoid desktop <you@example.com>
2025-05-28 15:04:04 -0600

    break publishers into their own scripts

---
+++ vision_pipeline/Publishers/LArmPublisher.py
@@ -0,0 +1,13 @@
+import time
+from RealsenseInterface import RealSenseCameraPublisher
+from unitree_sdk2py.core.channel import ChannelFactoryInitialize
+
+if __name__ == "__main__":
+    # Initialize the RealSense publisher
+    ChannelFactoryInitialize()
+    Larm_publisher = RealSenseCameraPublisher(channel_name='realsense/LArm', serial_number = "838212072778", InitChannelFactory=False)
+    start_time = time.time()
+    while True:
+        Larm_publisher.publish()
+        time.sleep(1/60)  # Publish at 60 Hz
+        print(f"Publishing at {(time.time() - start_time):.2f}s")
