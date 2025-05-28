07f9ed79f12545fb8c544a93859c316a4712e25b
humanoid desktop <you@example.com>
2025-05-28 16:25:25 -0600

    add explicit network interfaces

--- vision_pipeline/PublisherMain.py
+++ vision_pipeline/PublisherMain.py
@@ -1,15 +1,10 @@
 import time
 from unitree_sdk2py.core.channel import ChannelFactoryInitialize
-import sys
-import os
-
-# Add parent directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from RealsenseInterface import RealSenseCameraPublisher
 
 if __name__ == "__main__":
     # Initialize the RealSense publisher
-    ChannelFactoryInitialize()
+    ChannelFactoryInitialize(id = 0, networkInterface="enx00e04c6803cc")
     Head_publisher = RealSenseCameraPublisher(channel_name='realsense/Head', serial_number = "250122072330", InitChannelFactory=False)
     Larm_publisher = RealSenseCameraPublisher(channel_name='realsense/LArm', serial_number = "838212072778", InitChannelFactory=False)
     start_time = time.time()
