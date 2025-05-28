747f6d803512fa6305ea852eddb6d0fd1a27f353
humanoid desktop <you@example.com>
2025-05-28 14:52:31 -0600

    small change to RS publisher main

--- vision_pipeline/RS_Publisher_Main.py
+++ vision_pipeline/RS_Publisher_Main.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     ChannelFactoryInitialize()
     Head_publisher = RealSenseCameraPublisher(channel_name='realsense/Head', serial_number = "250122072330", InitChannelFactory=False)
     Larm_publisher = RealSenseCameraPublisher(channel_name='realsense/LArm', serial_number = "838212072778", InitChannelFactory=False)
-
+    start_time = time.time()
     while True:
         Head_publisher.publish()
         Larm_publisher.publish()
         time.sleep(1/60)  # Publish at 60 Hz
+        print(f"Publishing at {(time.time() - start_time):.2f}s")
