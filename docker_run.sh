eb8a3dc852604b97e93bf82d7da2753ef2eba35d
maxconway <maxlconway@gmail.com>
2025-06-06 14:03:21 -0600

    docker stuff

--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 xhost +local:root
 
-docker run --rm -it\
+docker run --rm -it \
+  --gpus all \
+  --shm-size=1g \
   --network host \
   -e DISPLAY="$DISPLAY" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /etc/timezone:/etc/timezone:ro \
   vision_pipeline:humble \
   "$@"
 
