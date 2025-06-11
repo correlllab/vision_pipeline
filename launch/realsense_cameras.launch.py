ee0c3609eaac14da2a49ac13514f7acb8985f41a
maxconway <maxlconway@gmail.com>
2025-06-11 11:55:00 -0600

    support launching different resolutions for different cameras

--- launch/realsense_cameras.launch.py
+++ launch/realsense_cameras.launch.py
@@ -10,24 +10,18 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     # Declare launch arguments for parameterization
     declared_arguments = [
-        DeclareLaunchArgument('width', default_value='320'),
-        DeclareLaunchArgument('height', default_value='240'),
-        DeclareLaunchArgument('fps', default_value='6'),
         DeclareLaunchArgument('enable_pointcloud', default_value='false'),
         DeclareLaunchArgument('enable_rgbd', default_value='false'),
     ]
 
     # Launch configurations
-    width = LaunchConfiguration('width')
-    height = LaunchConfiguration('height')
-    fps = LaunchConfiguration('fps')
     pointcloud = LaunchConfiguration('enable_pointcloud')
     rgbd = LaunchConfiguration('enable_rgbd')
 
     pkg_share = get_package_share_directory('realsense2_camera')
     rs_launch_file = os.path.join(pkg_share, 'launch', 'rs_launch.py')
 
-    def make_camera(name, serial):
+    def make_camera(name, serial, width, height, fps):
         return IncludeLaunchDescription(
             PythonLaunchDescriptionSource(rs_launch_file),
             launch_arguments={
@@ -45,7 +39,7 @@ def generate_launch_description():
         )
 
     return LaunchDescription(declared_arguments + [
-        #make_camera('head', '_250122072330'),
-        make_camera('left_hand', '_838212072778'),
-        #make_camera('right_hand', '_926522071700'),
+        make_camera('head', '_250122072330', width=424, height=240, fps=6),
+        make_camera('left_hand', '_838212072778', width=320, height=240, fps=6),
+        make_camera('right_hand', '_926522071700', width=320, height=240, fps=6),
     ])
