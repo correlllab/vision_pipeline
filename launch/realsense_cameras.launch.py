69d9082a8d21606cc8ec6e6c1f881b96281bf73e
maxconway <maxlconway@gmail.com>
2025-06-11 12:36:46 -0600

    parameterize launch file further

--- launch/realsense_cameras.launch.py
+++ launch/realsense_cameras.launch.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
@@ -17,39 +18,40 @@ def generate_launch_description():
         DeclareLaunchArgument('launch_right_hand', default_value='true')
     ]
 
-
-    launch_head = LaunchConfiguration('launch_head')
-    launch_left_hand = LaunchConfiguration('launch_left_hand')
-    launch_right_hand = LaunchConfiguration('launch_right_hand')
-    print(f"Launching cameras: head={launch_head}, left_hand={launch_left_hand}, right_hand={launch_right_hand}")
-
     # Launch configurations
     pointcloud = LaunchConfiguration('enable_pointcloud')
     rgbd = LaunchConfiguration('enable_rgbd')
+    launch_head = LaunchConfiguration('launch_head')
+    launch_left_hand = LaunchConfiguration('launch_left_hand')
+    launch_right_hand = LaunchConfiguration('launch_right_hand')
 
     pkg_share = get_package_share_directory('realsense2_camera')
     rs_launch_file = os.path.join(pkg_share, 'launch', 'rs_launch.py')
 
-    def make_camera(name, serial, width, height, fps):
-        return IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(rs_launch_file),
-            launch_arguments={
-                'camera_name': name,
-                'camera_namespace': 'realsense',
-                'serial_no': serial,
-                "rgb_camera.color_profile":f"{width},{height},{fps}",
-                'enable_color': 'true',
-                'enable_depth': 'true',
-                'pointcloud.enable': pointcloud,
-                'align_depth.enable': 'true',
-                'enable_rgbd': rgbd,
-                'enable_sync': 'true',
-            }.items()
+    def make_camera(name, serial, width, height, fps, condition):
+        return GroupAction(
+            actions=[
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(rs_launch_file),
+                    launch_arguments={
+                        'camera_name': name,
+                        'camera_namespace': 'realsense',
+                        'serial_no': serial,
+                        'rgb_camera.color_profile': f'{width},{height},{fps}',
+                        'enable_color': 'true',
+                        'enable_depth': 'true',
+                        'pointcloud.enable': pointcloud,
+                        'align_depth.enable': 'true',
+                        'enable_rgbd': rgbd,
+                        'enable_sync': 'true',
+                    }.items()
+                )
+            ],
+            condition=IfCondition(condition)
         )
 
-    cameras = []
-    cameras.append(make_camera('head', '_250122072330', width=424, height=240, fps=6)) if launch_head=="true" else None
-    cameras.append(make_camera('left_hand', '_838212072778', width=320, height=240, fps=6)) if launch_left_hand=="true" else None
-    cameras.append(make_camera('right_hand', '_926522071700', width=320, height=240, fps=6)) if launch_right_hand=="true" else None
-
-    return LaunchDescription(declared_arguments + cameras)
+    return LaunchDescription(declared_arguments + [
+        make_camera('head', '_250122072330', width=424, height=240, fps=6, condition=launch_head),
+        make_camera('left_hand', '_838212072778', width=320, height=240, fps=6, condition=launch_left_hand),
+        make_camera('right_hand', '_926522071700', width=320, height=240, fps=6, condition=launch_right_hand),
+    ])
