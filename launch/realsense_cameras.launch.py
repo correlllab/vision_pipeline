6d7100b729690294eeffc51af9e697f84c579c92
maxconway <maxlconway@gmail.com>
2025-07-16 15:10:06 -0600

    rs_launch uses config.json

--- launch/realsense_cameras.launch.py
+++ launch/realsense_cameras.launch.py
@@ -6,29 +6,25 @@ from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+import json
 
-
+config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'vision_pipeline', 'config.json'))
+config = json.load(open(config_path))
 def generate_launch_description():
     # Declare launch arguments for parameterization
     declared_arguments = [
         DeclareLaunchArgument('enable_pointcloud', default_value='false'),
         DeclareLaunchArgument('enable_rgbd', default_value='false'),
-        DeclareLaunchArgument('launch_head', default_value='true'),
-        DeclareLaunchArgument('launch_left_hand', default_value='true'),
-        DeclareLaunchArgument('launch_right_hand', default_value='true')
     ]
 
     # Launch configurations
     pointcloud = LaunchConfiguration('enable_pointcloud')
     rgbd = LaunchConfiguration('enable_rgbd')
-    launch_head = LaunchConfiguration('launch_head')
-    launch_left_hand = LaunchConfiguration('launch_left_hand')
-    launch_right_hand = LaunchConfiguration('launch_right_hand')
 
     pkg_share = get_package_share_directory('realsense2_camera')
     rs_launch_file = os.path.join(pkg_share, 'launch', 'rs_launch.py')
 
-    def make_camera(name, serial, width, height, fps, condition):
+    def make_camera(name, serial, width, height, fps):
         return GroupAction(
             actions=[
                 IncludeLaunchDescription(
@@ -47,11 +43,16 @@ def generate_launch_description():
                     }.items()
                 )
             ],
-            condition=IfCondition(condition)
         )
-
-    return LaunchDescription(declared_arguments + [
-        make_camera('head', '_250122072330', width=424, height=240, fps=6, condition=launch_head),
-        make_camera('left_hand', '_838212072778', width=424, height=240, fps=6, condition=launch_left_hand),
-        make_camera('right_hand', '_926522071700', width=424, height=240, fps=6, condition=launch_right_hand),
-    ])
+    cams = []
+    for camera_name, serial, frame in zip(config['rs_names'], config['rs_serials'], config['rs_frames']):
+        cams.append(
+            make_camera(
+                camera_name,
+                serial,
+                width = config['rs_width'],
+                height = config['rs_height'],
+                fps = config['rs_fps'],
+            )
+        )
+    return LaunchDescription(declared_arguments + cams)
